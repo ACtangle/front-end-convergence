1bcbfc0ff65a81f7230b27042698e6b23f3a9497
ACtangle <674253854@qq.com>
2020-07-27 13:15:42 +0800

    修改VO为单例模式

--- 0725卢涌佳前后端交互2/整体代码/photo/VO/ResponseVO.js
+++ 0725卢涌佳前后端交互2/整体代码/photo/VO/ResponseVO.js
@@ -1,12 +1,22 @@
-
 // 后端返回前端交互数据类
+// 单例模式
 
-module.exports = class ResponseVO{
-
-  constructor(state, msg ,data) {
-    this.state = state || -1;
-    this.msg = msg || '';
-    this.data = data || {};
+class ResponseVO {
+  constructor(state, msg, data) {
+    if (ResponseVO.instance) {
+      ResponseVO.instance.state = state;
+      ResponseVO.instance.msg = msg;
+      ResponseVO.instance.data = data;
+      console.log("second");
+      return ResponseVO.instance;
+    }
+    // 单例
+    ResponseVO.instance = this;
+    this.state = state;
+    this.msg = msg;
+    this.data = data;
+    console.log("first");
   }
-
 }
+
+module.exports = ResponseVO;
